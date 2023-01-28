3d318e6338592b3122b7d7e12243abd03753a81d
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2023-01-28 15:31:00 -0500

    Define API routes for messages

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
+import os
+import requests
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+# environment variables
+os.environ['bot_id'] = ''
+
+@app.route("/messages", methods=['POST'])
+def messages():
+  data = request.get_json()
+  print(data)
+
+  # check if message (data) is hate speech and respond
+  if data['sender_type'] != 'bot':
+    checkHateSpeech(data)
+
+  return 200
+
+# check for hate speech, if it is respond
+def checkHateSpeech(data):
+  hateSpeech = True #placeholder, just marking everything as hate speech
+
+  if (hateSpeech):
+    response('@' + data['name'] + ' watch your language. Your message has been flagged for hate speech.')
+
+# function used to send messages to groupme api
+def response(message):
+  url = 'https://api.groupme.com/v3/bots/post'
+  data = {'bot_id': os.getenv('bot_id'), 'text': message}
+  request = requests.post(url, data=data)
 
 if __name__ == "__main__":
   app.run()
